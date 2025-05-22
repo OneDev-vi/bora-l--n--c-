c9cd0e303da7a31d9491e10aa906ae971e5e1363
Niko <Niko-Tiro-Unico>
2025-05-22 15:59:03 -0300

    modificando o arquivo da aula4

--- aulas/aula4/aula4.cpp
+++ aulas/aula4/aula4.cpp
@@ -6,19 +6,29 @@ using namespace std;
 int main() {
     //tops de Variaveis heeepyyyyy
     //essa partes eu ja sei mas estou recomeçando o estudo para verse não perdi nada
-    int inteiros = 10;
-    float ponto = 10.32;
-    double pontos = 10.3245690;
-    bool VOrF = 0;
-    char s = 'a';
-    string sal = "Manga com sal é bom";
-    cout << inteiros << "\n" << ponto  << "\n" << pontos << "\n" << VOrF << " = false" << "\n" << s << "\n" << sal << endl;
+    int inteiros;
+    float ponto;
+    double pontos;
+    bool VOrF;
+    char s;
+    string sal;
+    char array[11]{};
     //e nessa parte quando você ver um array é porque eu ja estudei outras linguagens ate o mediano tipi C#
-    char array[11]{"sal é bom"};
-    cout << array << endl;
-    //agora tira mão de teclado!!
-    int manga = 0;
-    cout << "Dijite algum Numero!!" << endl;
-    cin >> manga;
-    cout << "esse foi o numero que você digitol: " << manga << endl;
+    //agora tira mão desse teclado!!
+    cout << "dijite numero(s) inteiros para colocar na variavel\n";
+    cin >> inteiros;
+    cout << "djite um numero com ponto tipo:4.32\n";
+    cin >> ponto;
+    cout << "dijite um numero com mais duas casas de pois do ponto\n";
+    cin >> pontos;
+    cout << "Dijite um ou zero\n";
+    cin >> VOrF;
+    cout << "Dijite APENAS uma letra\n";
+    cin >> s;
+    cout << "Dijite qualquer coisa com mais de uma letra\n";
+    cin >> sal;
+    cout << "Dijite mais de uma vez qualquer coisa com MENOS de 10 caracteres\n";
+    cin >> array;
+    cout << "Esse são o valores que você atribuiu as variaveis\n";
+    cout << inteiros << "\n" << ponto  << "\n" << pontos << "\n" << VOrF << "\n" << s << "\n" << sal << "\n" << array << endl;
 }
