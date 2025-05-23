85a600b19b1fd6edb040af3896449929f89b68d3
Niko <Niko-Tiro-Unico>
2025-05-23 13:30:58 -0300

    sal de novo

---
+++ aulas/aula5/aula.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define pistola 16
+#define shotgun 2
+#define mouser 10
+#define sal cout << "manga" << endl;
+
+int main() {
+    //declaração multipla
+    int horas, minutos, segundos, milesimos; //sinceramente prefiro declarar as vaiaveis em cada linha
+    //const int ID = 268914518; //ID de um produto
+    sal; //é meio estranho isso mais funciona...
+    return 0;
+}
